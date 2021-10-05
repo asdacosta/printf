3c20e1241707c2d5be11b53d1a8ae35ea941a08f
Dacosta Abraham Silvanus <dacostasilvanus@gmail.com>
2021-10-05 14:20:02 -0700

    Updared _printf.c file

--- _printf.c
+++ _printf.c
@@ -12,7 +12,10 @@ int _printf(const char *format, ...)
 	va_list arguments;
 	int (*function)(va_list, char *, unsigned int);
 	char *buffer;
+
 	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
 	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
 		return (-1);
+	if (!format[i])
+		return (0);
 }
