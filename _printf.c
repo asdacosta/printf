41ace8c448cc387d5638e6242c6ce34c2da11c15
asdacosta <dacostasilvanus@gmail.com>
2022-02-24 23:10:42 +0000

    Updated: Appended final code and return to produce formatted output

--- _printf.c
+++ _printf.c
@@ -41,5 +41,12 @@ int _printf(const char *format, ...)
 					}
 				} i++;
 			}
+			else
+				handl_buf(buffer, format[i], ibuf), len++;
+			for (ibuf = len; ibuf > 1024; ibuf -= 1024)
+				;
+		}
+		print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+		return (len);
 }
 
